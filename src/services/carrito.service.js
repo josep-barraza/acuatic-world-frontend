edfa3be40f65c448ed3f23933bfb00e7dd374668
jospeh-barraza <barraza.josesph@gmail.com>
2026-02-02 23:58:24 -0300

    repararCarrito3

--- src/services/carrito.service.js
+++ src/services/carrito.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_URL } from "../config/api";
+import { API_URL } from "../config/api.js"
 
 const API = `${API_URL}/aquaticWorld`; 
 
--- src/services/cursosService.js
+++ src/services/cursosService.js
@@ -5,7 +5,7 @@ import { API_URL } from "../config/api.js"
 
 const API = `${API_URL}/aquaticWorld`;
 
-// ✅ COMPRAR CURSO
+
 export const comprarCursoRequest = (cursoId, token) => {
   return axios.post(
     `${API}/cursos/comprar`,
@@ -18,7 +18,6 @@ export const comprarCursoRequest = (cursoId, token) => {
   );
 };
 
-// ✅ OBTENER CURSOS DEL USUARIO (PERFIL)
 export const getCursosUsuarioRequest = (token) =>
   axios.get(`${API}/usuarios/cursos`, {
     headers: {
