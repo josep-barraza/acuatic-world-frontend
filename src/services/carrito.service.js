63c793ce558032552759fc889c1344bc396c55e3
jospeh-barraza <barraza.josesph@gmail.com>
2026-02-02 22:26:40 -0300

    repararCarrito1

--- src/services/carrito.service.js
+++ src/services/carrito.service.js
@@ -13,6 +13,6 @@ export const agregarACarritoRequest = (productoId, token) =>
   );
 
 export const obtenerCarritoRequest = (token) =>
-  axios.post(`${API}/carrito`, {
+  axios.get(`${API}/carrito`, {
     headers: { Authorization: `Bearer ${token}` }
   });
